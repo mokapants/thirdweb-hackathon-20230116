eddf14df33df5f6abbddb9c81bf30429862e92cc
mokapants <mokafogeru@gmail.com>
2023-02-07 01:01:32 +0900

    Add: デバッグログ出力用のラップクラスを実装

---
+++ Unity/game-a/Assets/Scripts/Utils/Debug.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+    public static class Debug
+    {
+        public static void Log(string log)
+        {
+            UnityEngine.Debug.Log(log);
+        }
+    }
+}
