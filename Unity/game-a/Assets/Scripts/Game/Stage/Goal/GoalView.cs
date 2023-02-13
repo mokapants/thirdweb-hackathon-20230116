c407d2d0b02f8ea162b9b5b100687422dd00f2b9
mokapants <mokafogeru@gmail.com>
2023-02-13 20:08:05 +0900

    Add: ゴール時の演出を実装

---
+++ Unity/game-a/Assets/Scripts/Game/Stage/Goal/GoalView.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.Stage.Goal
+{
+    public class GoalView : MonoBehaviour
+    {
+        // --- メンバ変数 --- //
+        // ゴール時に表示するUIにつけるCanvasGroup
+        [SerializeField] private CanvasGroup goalCanvasGroup;
+
+        private void Start()
+        {
+            // ゴール演出を初期化
+            ResetGoalAnimation();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                OnTriggerEnterGoal();
+            }
+        }
+        
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                OnTriggerExitGoal();
+            }
+        }
+
+        /// <summary>
+        /// ゴールしたとき
+        /// </summary>
+        private void OnTriggerEnterGoal()
+        {
+            PlayGoalAnimation();
+        }
+        
+        /// <summary>
+        /// ゴールから離れたとき
+        /// </summary>
+        private void OnTriggerExitGoal()
+        {
+            ResetGoalAnimation();
+        }
+
+        /// <summary>
+        /// ゴール演出を再生
+        /// </summary>
+        private void PlayGoalAnimation()
+        {
+            goalCanvasGroup.alpha = 1;
+        }
+
+        /// <summary>
+        /// ゴール演出を初期状態にリセット
+        /// </summary>
+        private void ResetGoalAnimation()
+        {
+            goalCanvasGroup.alpha = 0;
+        }
+    }
+}
