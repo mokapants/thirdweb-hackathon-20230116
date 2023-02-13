be826ea16f135e880e9d05aa4960ca628e6f9ffd
mokapants <mokafogeru@gmail.com>
2023-02-13 22:57:19 +0900

    Fix: ビルド後はログを出力しないように変更

--- Unity/game-a/Assets/Scripts/Utils/Debug.cs
+++ Unity/game-a/Assets/Scripts/Utils/Debug.cs
@@ -5,13 +5,21 @@ namespace Utils
 {
     public static class Debug
     {
+#if UNITY_EDITOR
+        private static bool isOutput = true;
+#else
+        private static bool isOutput = false;
+#endif
+
         public static void Log(string log)
         {
+            if (!isOutput) return;
             UnityEngine.Debug.Log(log);
         }
-        
+
         public static void LogError(Exception exception)
         {
+            if (!isOutput) return;
             UnityEngine.Debug.LogError(exception);
         }
     }
